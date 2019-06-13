78a1acd64e4b3f0d326f060e6b77c0e2156826b7
Zergity <zergity@gmail.com>
2019-06-13 16:43:52 +0700

    the missing ingredients

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,11 @@
+var Unbiasable = artifacts.require('./Unbiasable.sol');
+
+const nullAddress = '0x0000000000000000000000000000000000000000'
+const truffleAddress = '0x95e2fcBa1EB33dc4b8c6DCBfCC6352f0a253285d'
+
+module.exports = async function(deployer) {
+    // 14541 iteration/2s-block
+    const realSpeed = 14541;
+    await deployer.deploy(Unbiasable, realSpeed, Math.floor(realSpeed/2))
+        .then(() => console.log(Unbiasable.address));
+};
